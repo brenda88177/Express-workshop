17a04dd1e560997182ce7c245094f115d504a0aa
BRENDA DANIELA PAREDES NUÑEZ <bparedes08@alumnos.uaq.mx>
2025-10-17 18:54:39 -0600

    17 DE OCTUBRE

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -4,21 +4,35 @@ const bd = require('../config/database');
 
 
 
-pokemon.post("/", (req, res, next)=>{
-    return res.status(200).send(req.body);
+pokemon.post("/", async (req, res, next)=>{
+    const {pok_name, pok_height, pok_weight, pok_base_experience } = req.body;
+    
+    if(pok_name&&pok_height&&pok_weight&& pok_base_experience ) {
+        let query="INSERT INTO pokemon (pok_name, pok_height, pok_weight, pok_base_experience)";
+        query += ` VALUES('${pok_name}´,${pok_height}, ${pok_weight}, ${pok_base_experience} )`;
+        
+        const rows = await db.query(query);
+        if(rows.affectedRows ==1){
+            return res.status(201).json({code:201,message: "pokemon insertado correctamente"});
+        }
+    return res.status(500).json({code:500, message: "ERROR"});
 
+    }
+    return res.status(500).json({code: 500, message:"CAMPOS INCOMPLETOS"});
+
+    
 });
 // Ruta para obtener todos los Pokémon
 pokemon.get("/", async (req, res) => {
     const pkmn = await db.query("SELECT * FROM pokemon");
-    res.status(200).json({code: 1,  message: pkmn});
+    res.status(200).json({code: 200,  message: pkmn});
 });
 
 // Ruta para obtener un Pokémon por ID
 pokemon.get("/:id", (req, res) => {
     const id = parseInt(req.params.id); // convertir a número
     if (id >= 1 && id <= 722) {
-        return res.status(200).json({code:1, message:pkmn});
+        return res.status(200).json({code:200, message:pkmn});
     } else {
         return res.status(404).send({ code:404, message: "Pokémon no encontrado" });
     }
