e20c490d19a091b3de8fbfede754b80bc3574bc9
BRENDA DANIELA PAREDES NUÑEZ <bparedes08@alumnos.uaq.mx>
2025-10-25 18:47:13 -0600

    25 DE OCTUBRE 6:50

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -23,6 +23,52 @@ pokemon.post("/", async (req, res, next)=>{
     
 });
 // Ruta para obtener todos los Pokémon
+
+pokemon.delete("/:id([0-9]{1,3})", async (req,res,next) => {
+    const query = `DELETE FROM pokemon WHERE pok_id=${req.params.id}`;
+    const rows = await db.query(query);
+
+    if(rows.affectedRows==1){
+        return res.status(200).json({code:200, message: "POKEMON BORRADO"});
+
+    }
+    return res.status(404).json({code:400, message:"POKEMON NO ENCONTRADO"});
+});
+
+
+pokemon.put("/:id([0-9]{1,3})", async (req,res,next) => {
+    const {pok_name, pok_height, pok_weight, pok_base_experience } = req.body;
+
+    if(pok_name&&pok_height&&pok_weight&& pok_base_experience ) {
+        let query = `UPDATE pokemon SET ,pok_name='${pok_name}',pok_heigh=${pok_heigh},`;
+    query += `pok_weight=$[pok_weight],pok_base_experience=${pok_base_experience} WHERE pok_id=${req.params.id};`;
+
+        const rows = await db.query(query);
+        if(rows.affectedRows ==1){
+            return res.status(200).json({code:201,message: "pokemon actualizado correctamente"});
+        }
+    return res.status(500).json({code:500, message: "ERROR"});
+
+    }
+    return res.status(500).json({code: 500, message:"CAMPOS INCOMPLETOS"});
+
+});
+
+pokemon.patch("/:id([0-9]{1,3})", async (req,res,next) => {
+    if(req.body.pok_name){
+        let query = `UPDATE pokemon SET ,pok_name='${req.bodypok_name}' WHERE pok_id=${req.params.id}`;
+    
+            const rows = await db.query(query);
+            if(rows.affectedRows ==1){
+                return res.status(200).json({code:201,message: "pokemon actualizado correctamente"});
+            }
+            return res.status(500).json({code:500, message:"POKEMON ACTUALIZADO"});
+            return res.status(500).json({code:500, message:"OCURRIO UN ERROR"});;
+        }
+        return res.status(500).json({code:500, message:"CAMPOS INCOMPLETOS"});;
+});
+
+
 pokemon.get("/", async (req, res) => {
     const pkmn = await db.query("SELECT * FROM pokemon");
     res.status(200).json({code: 200,  message: pkmn});
