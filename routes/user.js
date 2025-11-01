dcddc1215f8c07b1b83988a559b42ad5cd8ae37e
BRENDA DANIELA PAREDES NUÑEZ <bparedes08@alumnos.uaq.mx>
2025-10-31 20:35:46 -0600

    31 DE OCTUBRE

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const user = express.Router();
 const db = require('../config/database');
+const jwt = require('jsonwebtoken');
 
 user.post("/",async (req,res,next) => {
     const {user_name, user_mail, user_password}=req.body
@@ -18,6 +19,28 @@ user.post("/",async (req,res,next) => {
     }
     return res.status(500).json({code:500, message:"CAMPOS INCOMPLETOS"});
     
+});
+
+user.post("/login", async(req,res,next)=>{
+    const{user_mail, user_password} = req.body
+    const query = `SELECT * FROM user WHERE user_mail='${user_mail}' AND user_password=' ${user_password}';`
+    const rows = await db.query(query);
+
+    if(user_mail && user_password){
+        if(rows.length == 1) {
+            const token = jwt.sign({
+                user_id: rows[0].user_id,
+                user_mail: rows[0].user_mail
+            }, "debugkey");
+
+            return res.status(200).json({code:200, message:token});
+        }
+        else{
+            return res.status(401).json({code:401, message: "USUARIO INCORRECTO"});
+        }
+    }
+    return res.status(500).json({code:500, message: "CAMPOS INCOMPLETOS"});
+
 });
 
 user.get("/", async (req,res,next) => {
